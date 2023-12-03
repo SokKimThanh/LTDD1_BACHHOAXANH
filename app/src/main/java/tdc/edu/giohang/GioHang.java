63123d219a59880e2a77149976771f49bce0e2b0
Sok Kim Thanh <thanhsk1991@gmail.com>
2023-12-03 23:47:56 +0700

    add files update giohang

--- app/src/main/java/tdc/edu/giohang/GioHang.java
+++ app/src/main/java/tdc/edu/giohang/GioHang.java
@@ -9,15 +9,19 @@ import tdc.edu.danhsachsp.HangHoa;
 
 public class GioHang {
 
-    private HashMap<HangHoa, Integer> hangHoaMap;
+    private HashMap<HangHoa, Integer> giohang;
 
     public GioHang() {
-        hangHoaMap = new HashMap<>();
+        giohang = new HashMap<>();
     }
 
+    /**
+     * Thêm 1 sản phẩm vào danh sách sản phẩm trong hóa đơn
+     * @param hangHoa
+     */
     public void add(HangHoa hangHoa) {
-        int quantity = hangHoaMap.containsKey(hangHoa) ? hangHoaMap.get(hangHoa) : 0;
-        hangHoaMap.put(hangHoa, quantity + 1);
+        int quantity = giohang.containsKey(hangHoa) ? giohang.get(hangHoa) : 0;
+        giohang.put(hangHoa, quantity + 1);
     }
 
     /**
@@ -26,7 +30,7 @@ public class GioHang {
      * @return Phương thức này nhận vào một đối tượng HangHoa và trả về số lượng của sản phẩm đó trong giỏ hàng
      */
     public int getQuantity(HangHoa hangHoa) {
-        return hangHoaMap.containsKey(hangHoa) ? hangHoaMap.get(hangHoa) : 0;
+        return giohang.containsKey(hangHoa) ? giohang.get(hangHoa) : 0;
     }
 
     /**
@@ -35,31 +39,57 @@ public class GioHang {
      */
     public int getQuantity() {
         int totalQuantity = 0;
-        for (int quantity : hangHoaMap.values()) {
+        for (int quantity : giohang.values()) {
             totalQuantity += quantity;
         }
         return totalQuantity;
     }
-    // Các phương thức khác...
+
+    /**
+     * Đem danh sách sản phẩm trong hóa đơn (giỏ hàng) đi in cho người ta xem
+     * @return giỏ hàng sản phẩm(thanh toán hóa đơn)
+     */
     public List<HangHoa> getHangHoaList() {
         // Trả về một danh sách các sản phẩm trong giỏ hàng
-        return new ArrayList<>(hangHoaMap.keySet());
+        return new ArrayList<>(giohang.keySet());
     }
 
+    /**
+     * Kiểm tra sản phẩm trong giỏ hàng còn không
+     * @param hangHoa
+     * @return
+     */
     public boolean contains(HangHoa hangHoa) {
         // Kiểm tra xem một sản phẩm có trong giỏ hàng hay không
-        return hangHoaMap.containsKey(hangHoa);
+        return giohang.containsKey(hangHoa);
     }
 
+    /**
+     * Tăng số lượng sản phẩm trong giỏ hàng
+     * @param hangHoa
+     */
     public void increaseQuantity(HangHoa hangHoa) {
         // Tăng số lượng của một sản phẩm trong giỏ hàng
         int quantity = getQuantity(hangHoa);
-        hangHoaMap.put(hangHoa, quantity + 1);
+        giohang.put(hangHoa, quantity + 1);
     }
 
+    /**
+     * Giảm số lượng sản phẩm trong giỏ hàng
+     * @param hangHoa
+     */
+    public void decreaseQuantity(HangHoa hangHoa) {
+        // Tăng số lượng của một sản phẩm trong giỏ hàng
+        int quantity = getQuantity(hangHoa);
+        giohang.put(hangHoa, quantity - 1);
+    }
+    /**
+     * Tính tiền thanh toán giỏ hàng
+     * @return Tổng tiền giỏ hàng
+     */
     public double getTongThanhTien() {
         double tongThanhTien = 0;
-        for (Map.Entry<HangHoa, Integer> entry : hangHoaMap.entrySet()) {
+        for (Map.Entry<HangHoa, Integer> entry : giohang.entrySet()) {
             HangHoa hangHoa = entry.getKey();
             int quantity = entry.getValue();
             tongThanhTien += hangHoa.getGiaSp() * quantity;
